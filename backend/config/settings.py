583d60dc301169ca32a9538fe650f9ac3325eb1d
Stevy <superstevy@yahoo.com>
2021-08-25 19:04:36 -0400

    fixed api

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'zoomglam-frontend.herokuapp.com']
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000',
-                        'http://127.0.0.1:3000', 'https://zoomglam-frontend.herokuapp.com/']
+                        'http://127.0.0.1:3000', 'https://zoomglam-frontend.herokuapp.com']
 
 # Application definition
 
